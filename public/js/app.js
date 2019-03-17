7bf135cf98fbfd2a6bf784fe508823fcfdac16b9
Santosh <skskumar448@gmail.com>
2019-03-17 13:50:39 +0530

    js file changes

--- public/js/app.js
+++ public/js/app.js
@@ -10,13 +10,13 @@ weatherForm.addEventListener('submit', (e) => {
    messageOne.textContent = 'Loading.....';
    messageTwo.textContent = '';
    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-      response.text().then((data) => {
-          result = JSON.parse(data);
-          if(result.error) {
-              messageOne.textContent = result.error;
+      response.json().then((data) => {
+          //result = JSON.parse(data);
+          if(data.error) {
+              messageOne.textContent = data.error;
           } else {
-              messageOne.textContent = result.location;
-              messageTwo.textContent = result.forecast;
+              messageOne.textContent = data.location;
+              messageTwo.textContent = data.forecast;
           }
       })
 })
